b2510609eb07338bec78d8f3d4276d08fdadc793
Rafiw Idrissu <dev.rafiu@gmail.com>
2023-10-31 14:31:28 -0700

    config colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "rgba(var(--background),<alpha-value>)",
+        "backgound-secondary":
+          "rgba(var(--background-secondary,<alpha-value>))",
+
+        foreground: "rgba(var(--foreground),<alpha-value>)",
+        "foreground-muted": "rgba(var(--foreground-muted),<alpha-value>)",
+
+        secondary: "rgba(var(secondary),<alpha-value>)",
+
+        "pie-chart-teal": "rgba(var(--pie-chart-teal),<alpha-value>)",
+        "pie-chart-pastel-blue":
+          "rgba(var(--pie-chart-pastel-blue),<alpha-value>)",
+        "pie-chart-pink": "rgba(var(--pie-chart-pink),<alpha-value>)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
