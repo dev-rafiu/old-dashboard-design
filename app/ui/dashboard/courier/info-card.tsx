fd1ec6e19799a6812552a8278655445c4fe4135e
Rafiw Idrissu <dev.rafiu@gmail.com>
2023-11-03 15:09:04 -0700

    reduce padding value

--- app/ui/dashboard/courier/info-card.tsx
+++ app/ui/dashboard/courier/info-card.tsx
@@ -20,18 +20,18 @@ function InfoCard({ title, percentageInfo, percentageDiff, value }: Props) {
   return (
     // <div className="bg-gray-400 animate-pulse shadow-md h-40 rounded"></div>
     <Card>
-      <CardHeader className="flex-row gap-4 items-end justify-between p-4">
-        <CardTitle className="capitalize text-lg self-start text-foreground-custom/80">
+      <CardHeader className="flex-row gap-2 items-end justify-between p-4">
+        <CardTitle className="capitalize text-base self-start text-foreground-custom/80">
           {title}
         </CardTitle>
-        <span className="border p-2 rounded-full bg-secondary-custom/20 text-secondary-custom">
-          <GoArrowUpRight className="text-lg" />
+        <span className="flex items-center justify-center p-2 w-8 aspect-square rounded-full bg-secondary-custom/20 text-secondary-custom">
+          <GoArrowUpRight className="" />
         </span>
       </CardHeader>
-      <CardContent>
-        <p className="text-foreground-muted-custom flex items-center gap-2">
+      <CardContent className="p-3">
+        <p className="text-foreground-muted-custom flex gap-2 text-base">
           {percentageDiff == "up" ? (
-            <span className="text-[rgba(133,237,218,1)]">
+            <span className="text-[rgba(133,237,218,1)] self-start">
               <FaArrowTrendUp />
             </span>
           ) : (
@@ -42,7 +42,7 @@ function InfoCard({ title, percentageInfo, percentageDiff, value }: Props) {
           {percentageInfo}
         </p>
       </CardContent>
-      <CardFooter>
+      <CardFooter className="p-3">
         <p className="text-2xl font-semibold text-foreground-custom">{value}</p>
       </CardFooter>
     </Card>
